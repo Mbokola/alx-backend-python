bdc04040793f26522422b6bac2372b5b97810d5b
Mbokola <tonypeaks@gmail.com>
2023-12-11 21:37:20 +0000

    Return type

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Multiple coroutines """
     delays: List = []
     for _ in range(n):
