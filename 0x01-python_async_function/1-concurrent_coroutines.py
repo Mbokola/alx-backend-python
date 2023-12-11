baf3a49259f46365cd670ec6aabf3c111ec688c2
Mbokola <tonypeaks@gmail.com>
2023-12-11 21:12:15 +0000

    List of floats

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Multiple coroutines """
     delays: List = []
     for _ in range(n):
