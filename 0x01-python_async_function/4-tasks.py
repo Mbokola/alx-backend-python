daa111f2d2d5aeb1b25a4b4eb1c7d2fd6af48b37
Mbokola <tonypeaks@gmail.com>
2023-12-11 21:05:31 +0000

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 4-tasks module """
+
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List:
+    """ Multiple coroutines """
+    delays: List = []
+    for _ in range(n):
+        delays.append(await task_wait_random(max_delay))
+    return sorted(delays)
