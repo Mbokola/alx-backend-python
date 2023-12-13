1b7264d045f5aed720a9e94d2fad4f38922ce088
Mbokola <tonypeaks@gmail.com>
2023-12-13 14:10:58 +0000

    Typing change

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ 0-async_generator module """
 
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """ An async generator """
     for _ in range(10):
         await asyncio.sleep(1)
