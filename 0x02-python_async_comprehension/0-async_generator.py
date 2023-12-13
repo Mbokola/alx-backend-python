24feff0d5871bb1c14f100fe4a5d460bb40fb2b7
Mbokola <tonypeaks@gmail.com>
2023-12-13 14:02:22 +0000

    Docs

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ import random
 
 
 async def async_generator() -> AsyncGenerator:
+    """ An async generator """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ An async comrehension """
     return [i async for i in async_generator()]
